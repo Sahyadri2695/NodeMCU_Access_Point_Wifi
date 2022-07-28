69715a488f905d271b7aba8a03584f5d8d77b25d
Lingesh K <81001528+Sahyadri2695@users.noreply.github.com>
2022-07-28 19:56:44 +0530

    Update NodeMCU_Access_Point_Wifi.ino

--- NodeMCU_Access_Point_Wifi/NodeMCU_Access_Point_Wifi.ino
+++ NodeMCU_Access_Point_Wifi/NodeMCU_Access_Point_Wifi.ino
@@ -1,36 +1,24 @@
-/*
- * ESP8266 (NodeMCU) Handling Web form data basic example
- * https://circuits4you.com
- */
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP_EEPROM.h>
 
-#define MAX_STRING_LENGTH 32
+#define MAX_STRING_LENGTH 64
 
-//SSID and Password of your WiFi router
+boolean ok;
 const char* ssid = "Hello IoT";
 const char* password = "12345678";
-
 struct 
 { 
     String wifi_ssid;
     String wifi_password;
-    unsigned int IP1;
-    unsigned int IP2;
-    unsigned int IP3;
-    unsigned int IP4;
-    unsigned int GW1;
-    unsigned int GW2;
-    unsigned int GW3;
-    unsigned int GW4;
+    String IP;
+    String GW;
 } data;
 
-char result1[MAX_STRING_LENGTH];
-char result2[MAX_STRING_LENGTH];
+String IPAddr;
+String Gateway;
 
-boolean ok;
 int addr = 0x00;
 uint8_t retries=0;
 
@@ -48,25 +36,12 @@ String A =  R"=====(
   <br><br>
   IP:
   <br>
-  <input type="text" name="IP1" min="0" max="255" value="">
-  .
-  <input type="text" name="IP2" min="0" max="255" value="">
-  .
-  <input type="text" name="IP3" min="0" max="255" value="">
-  .
-  <input type="text" name="IP4" min="0" max="255" value="">
+  <input type="text" name="IP" value="">
   <br>
   <br>
   Gateway:
   <br>
-  <input type="text" name="GW1" min="0" max="255" value="">
-  .
-  <input type="text" name="GW2" min="0" max="255" value="">
-  .
-  <input type="text" name="GW3" min="0" max="255" value="">
-  .
-  <input type="text" name="GW4" min="0" max="255" value="">
-  <br><br>
+  <input type="text" name="GW" value="">
   <input type="submit" value="Submit">
 </form> 
 </body>
@@ -75,128 +50,64 @@ String A =  R"=====(
 
 ESP8266WebServer server(80); //Server on port 80
 
-//===============================================================
-// This routine is executed when you open its IP in browser
-//===============================================================
 void handleRoot()
 {
   //Read HTML contents
  server.send(200, "text/html", A); //Send web page
 }
-//===============================================================
-// This routine is executed when you press submit
-//===============================================================
 void handleForm()
 {
-  data.wifi_ssid = server.arg("SSID"); 
-  data.wifi_password = server.arg("Password"); 
-  
-  String ip1 = server.arg("IP1");
-  data.IP1 = ip1.toInt();
-  String ip2 = server.arg("IP2");
-  data.IP2 = ip2.toInt();
-  String ip3 = server.arg("IP3");
-  data.IP3 = ip3.toInt();
-  String ip4 = server.arg("IP4");
-  data.IP4 = ip4.toInt();
-
-  String gw1 = server.arg("GW1");
-  data.GW1 = gw1.toInt();
-  String gw2 = server.arg("GW2");
-  data.GW2 = gw2.toInt();
-  String gw3 = server.arg("GW3");
-  data.GW3 = gw3.toInt();
-  String gw4 = server.arg("GW4");
-  data.GW4 = gw4.toInt();
-  
-  int x = sizeof(data.wifi_ssid);
-  int y = data.wifi_ssid.length();
-  int k = 0;
-  memset(result1, '\0', sizeof(result1));
-  memset(result2, '\0', sizeof(result2));
-
-  for(int i = 0; i < data.wifi_ssid.length(); i++)
-  {
-    EEPROM.write((addr + i), data.wifi_ssid[i]); 
-    ok = EEPROM.commit();
-    Serial.println((ok) ? "OK1" : "Commit failed");
-    //Serial.println(EEPROM.read(addr + i));
-    result1[i] = EEPROM.read(addr + i);
-  }
-  result1[data.wifi_ssid.length()+1] = '\0';
-  
-  for(int j = (data.wifi_ssid.length())+2; k < data.wifi_password.length(); j++)
-  {
-    //Serial.println("Inside 2nd for loop");
-    EEPROM.write((addr + j), data.wifi_password[k]); 
-    ok = EEPROM.commit();
-    Serial.println((ok) ? "OK2" : "Commit failed");
-    //Serial.println(EEPROM.read(addr + j));
-    result2[k] = EEPROM.read(addr + j);
-    k++;
-  }
-  result2[data.wifi_password.length()+1] = '\0';
-
+  EEPROM_write();
   String s = "<a href='/'> Go Back </a>";
   server.send(200, "text/html", A); //Send web page
-  StationMode(result1, result2);
+  stationmode();
 }
 
-void StationMode (char* STA_SSID, char* STA_PW)
+void EEPROM_write()
 {
-  WiFi.mode(WIFI_STA);
-
-  IPAddress local_IP(data.IP1, data.IP2, data.IP3, data.IP4);
-  IPAddress gateway(data.GW1, data.GW2, data.GW3, data.GW4);
-
-  IPAddress subnet(255, 255, 0, 0);
-  IPAddress primaryDNS(8, 8, 8, 8);   //optional
-  IPAddress secondaryDNS(8, 8, 4, 4); //optional
-
-  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) 
-  {
-    Serial.println("STA Failed to configure");
-  }
-
-  WiFi.begin(STA_SSID, STA_PW);
-  Serial.print("Connecting to ");
-  Serial.print(STA_SSID);
-  Serial.println("...");
-
-  //Wait for WiFi to connect for a maximum timeout of 20 seconds
-  while(WiFi.status()!=WL_CONNECTED && retries<20)
-  {
-    Serial.print(".");
-    retries++;
-    delay(1000);
-  }
- 
-  Serial.println();
-  //Inform the user whether the timeout has occured, or the ESP8266 is connected to the internet
-  if(retries==20)//Timeout has occured
-  {
-    Serial.print("Unable to Connect to ");
-    Serial.println(STA_SSID);
-  }
-   
-  if(WiFi.status()==WL_CONNECTED)//WiFi has succesfully Connected
-  {
-    Serial.print("Successfully connected to ");
-    Serial.println(STA_SSID);
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
-  }
+  int str1AddrOffset = writeStringToEEPROM(addr, server.arg("SSID"));
+  int str2AddrOffset = writeStringToEEPROM(str1AddrOffset, server.arg("Password"));
+  int str3AddrOffset = writeStringToEEPROM(str2AddrOffset, server.arg("IP"));
+  int str4AddrOffset = writeStringToEEPROM(str3AddrOffset, server.arg("GW"));
+}
+void EEPROM_read()
+{
+  int newStr1AddrOffset = readStringFromEEPROM(addr, &data.wifi_ssid);
+  int newStr2AddrOffset = readStringFromEEPROM(newStr1AddrOffset, &data.wifi_password);
+  int newStr3AddrOffset = readStringFromEEPROM(newStr2AddrOffset, &data.IP);
+  int newStr4AddrOffset = readStringFromEEPROM(newStr3AddrOffset, &data.GW);
+  Serial.println(data.IP);
+  Serial.print(data.GW);
 }
 
-void APMode (void)
+int writeStringToEEPROM(int addrOffset, const String &strToWrite)
 {
-  
+  byte len = strToWrite.length();
+  EEPROM.write(addrOffset, len);
+  for (int i = 0; i < len; i++)
+  {
+    EEPROM.write(addrOffset + 1 + i, strToWrite[i]);
+    ok = EEPROM.commit();
+    Serial.println((ok) ? "OK2" : "Commit failed");
+  }
+  return addrOffset + 1 + len;
 }
 
-void setup(void)
+int readStringFromEEPROM(int addrOffset, String *strToRead)
 {
-  Serial.begin(9600);
-  EEPROM.begin(64);
+  int newStrLen = EEPROM.read(addrOffset);
+  char data[newStrLen + 1];
+  for (int i = 0; i < newStrLen; i++)
+  {
+    data[i] = EEPROM.read(addrOffset + 1 + i);
+  }
+  data[newStrLen] = '\0'; 
+  *strToRead = String(data);
+  return addrOffset + 1 + newStrLen;
+}
+
+void softapmode()
+{
   WiFi.mode(WIFI_AP);           //Only Access point
   WiFi.softAP(ssid, password);  //Start HOTspot removing password will disable security
   IPAddress myIP = WiFi.softAPIP(); //Get IP address
@@ -209,18 +120,48 @@ void setup(void)
   Serial.println("WiFi");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());  //IP address assigned to your ESP
- 
+
   server.on("/", handleRoot);      //Which routine to handle at root location
   server.on("/action_page", handleForm); //form action is handled here
 
   server.begin();                  //Start server
   Serial.println("HTTP server started");
 }
-//==============================================================
-//                     LOOP
-//==============================================================
-void loop(void)
+void stationmode()
+{
+  uint8_t retries=0;
+  EEPROM_read();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(data.wifi_ssid, data.wifi_password);
+  while(WiFi.status()!=WL_CONNECTED && retries<20)
+  {
+    Serial.print(".");
+    retries++;
+    delay(1000);
+  }
+  Serial.println();
+  //Inform the user whether the timeout has occured, or the ESP8266 is connected to the internet
+  if(retries==20)//Timeout has occured
+  {
+    Serial.print("Unable to Connect to ");
+    Serial.println(data.wifi_ssid);
+  }
+  if(WiFi.status()==WL_CONNECTED)//WiFi has succesfully Connected
+  {
+    Serial.print("Successfully connected to ");
+    Serial.println(data.wifi_ssid);
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+  }
+}
+void setup() 
+{
+  Serial.begin(9600);
+  EEPROM.begin(sizeof(data));
+  softapmode();
+}
+
+void loop() 
 {
   server.handleClient();          //Handle client requests
 }
-  
